67d6bac4368265f9faf5b23d704aab35db190478
djemalzecevic <djemal.zecevic@gmail.com>
2014-02-26 21:30:45 +0100

    CallPhoe
    Add  call phone popup

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,69 @@
+;(function ($, window, undefined) {
+  'use strict';
+
+  var $doc = $(document),
+      Modernizr = window.Modernizr;
+
+  $(document).ready(function() {
+    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
+    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
+
+    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
+  });
+
+  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+  if (Modernizr.touch && !window.location.hash) {
+    $(window).load(function () {
+      setTimeout(function () {
+        window.scrollTo(0, 1);
+      }, 0);
+    });
+  }
+
+})(jQuery, this);
+
+$(function(){
+    var $write = $('#write');
+    
+    $(document).ready(function() {
+        $('button').click(function() {
+        	var $this = $(this),
+            character = $this.html();
+        	
+        	// Delete
+        	if (character == 'Reset' ) {
+        	    var html = $write.html();
+        	     
+        	    $write.html(html.substr(0, html.length - 1));
+        	    return false;
+        	}
+        	
+        	
+        	$write.html($write.html() + character);
+        });
+    });
+});
+
+function closeModal() {
+  // The popup is powered by the Reveal plugin
+  $("#add-quote").trigger("reveal:close");
+}
+
 $(function() {
     // Initialize Parse SDK
 	// ZD 26.01.14 From parse.com first one is ApplicationID and the second one is JavaScript ID key
@@ -44,11 +110,11 @@ $(function() {
         initFromUser(Parse.User.current());
         $('#main').fadeIn();
     } else {
-        $('#login').fadeIn();
+        $('#container').fadeIn();//$('#login').fadeIn();
     }
 
     // Logic to handle the login form
-    var $loginForm = $('#loginForm');
+    var $loginForm = $('#form-signin'); //$('#loginForm');
     $loginForm.submit(function(e) {
         e.preventDefault();
         Parse.User.logIn(
@@ -59,8 +125,8 @@ $(function() {
                     // Set up user name for current user
                     initFromUser(user);
 
-                    // Manipulate UI
-                    $('#login').fadeOut(function() {
+                    // Manipulate UI $('#login').fadeOut(function()
+                    $('#container').fadeOut(function() {
                         $('#main').fadeIn();
                         $loginForm.find('input[type=text]').val('');
                         $loginForm.find('input[type=password]').val('');
@@ -77,7 +143,7 @@ $(function() {
     $('#logout').on('click', function(e) {
         Parse.User.logOut();
         $('#main').fadeOut(function() {
-            $('#login').fadeIn();
+            $('#container').fadeIn(); //$('#login').fadeIn();
         });
     });
 
